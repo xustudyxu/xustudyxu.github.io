f1a686ac9811963599d99cd56b1ca250ca8506a2
xustudyxu <1812903531@qq.com>
2022-03-08 22:08:35 +0800

    Source code

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -5,21 +5,21 @@ module.exports = [
       {
         text: 'Java基础',
         items: [
-          { text: 'JavaSE', link: '/studynotes/Java/one_' }
+          { text: 'JavaSE', link: '/studynotes/Java/one_/' }
         ]
       }, {
         text: 'JavaWeb',
         items: [
-          { text: 'Servlet', link: '/studynotes/Servlet/one' }
+          { text: 'Servlet', link: '/studynotes/Servlet/one/' }
 
         ]
       },
       {
         text: '主流框架',
         items: [
-          { text: 'Spring5', link: '/studynotes/Spring5/one' },
-          { text: 'SpringMVC', link: '/studynotes/SpringMVC/one' },
-          { text: 'Mybatis', link: '/studynotes/Mybatis/index_' }
+          { text: 'Spring5', link: '/studynotes/Spring5/one/' },
+          { text: 'SpringMVC', link: '/studynotes/SpringMVC/one/' },
+          { text: 'Mybatis', link: '/studynotes/Mybatis/index_/' }
         ]
       }
     ]
@@ -29,8 +29,8 @@ module.exports = [
       {
         text: 'Python',
         items: [
-          { text: 'Python基础', link: '/studynotes/Python/python01' },
-          { text: '数据分析', link: '/studynotes/dataCleaning/one' }
+          { text: 'Python基础', link: '/studynotes/Python/python01/' },
+          { text: '数据分析', link: '/studynotes/dataCleaning/one/' }
         ]
       },
   
@@ -41,12 +41,12 @@ module.exports = [
     items: [
       { text: 'SQL 数据库', 
         items:[
-          {text: 'MySQL', link: '/studynotes/MySQL/MySQL' }
+          {text: 'MySQL', link: '/studynotes/MySQL/MySQL/' }
         ]
       },
       { text: 'NoSQL 数据库', 
       items:[
-        {text: 'Redis', link: '/studynotes/Redis/one' }
+        {text: 'Redis', link: '/studynotes/Redis/one/' }
       ]
     },
     ]
@@ -55,18 +55,18 @@ module.exports = [
   {
     text: '项目管理|部署',
     items: [
-      { text: 'Linux', link: '/studynotes/Linux/1' },
-      { text: 'Maven', link: '/studynotes/Maven/one' },
-      { text: 'Git', link: '/studynotes/Git/Git_' },
+      { text: 'Linux', link: '/studynotes/Linux/1/' },
+      { text: 'Maven', link: '/studynotes/Maven/one/' },
+      { text: 'Git', link: '/studynotes/Git/Git_/' },
     ]
   },
 
   {
     text: '面试宝典',
     items: [
-      { text: '力扣算法题', link: '/baodian/LeetCode/easy_' },
-      { text: '初级开发篇', link: '/baodian/zero/one' },
-      { text: '中高进阶篇', link: '/baodian/high/one' },
+      { text: '力扣算法题', link: '/baodian/LeetCode/easy_/' },
+      { text: '初级开发篇', link: '/baodian/zero/one/' },
+      { text: '中高进阶篇', link: '/baodian/high/one/' },
   
     ]
   },
