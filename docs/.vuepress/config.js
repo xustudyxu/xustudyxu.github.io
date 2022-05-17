7990fa239fd3f1177abae201e3b07654626a40d8
xustudyxu <1812903531@qq.com>
2022-05-17 20:00:35 +0800

    :wrench:添加内容:rocket:

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,7 +54,7 @@ module.exports = {
     // // 默认是 false, 设置为 true 来启用
 
     //首页大图
-    bodyBgImg: 'https://cdn.jsdelivr.net/gh/xustudyxu/image-hosting@master/image.l26t2xoub34.webp',  // 你的图片路径(必须位于 public 下)，可以是 URL
+    bodyBgImg: 'https://cdn.jsdelivr.net/gh/xustudyxu/image-hosting@master/image.79hde46t2200.webp',  // 你的图片路径(必须位于 public 下)，可以是 URL
     bodyBgImgOpacity: 1, // body 背景图透明度，选值 0 ~ 1.0, 默认0.5
 
     serviceWorker: {
