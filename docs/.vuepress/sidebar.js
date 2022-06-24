b5cdb699f9b1e32c8ca03cad05a94f1150dabe65
xustudyxu <1812903531@qq.com>
2022-06-24 11:35:45 +0800

    :wrench:添加内容:rocket:

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  //对多模块的管控
+      //对多模块的管控
      '/studynotes/Java/':require('../studynotes/Java/sidebar'),
      '/studynotes/database/':require('../studynotes/database/sidebar'),
      '/studynotes/project-management/':require('../studynotes/project-management/sidebar'),
