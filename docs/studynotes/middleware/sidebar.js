32d5ecc191cf0549e5cc20f59024f147e167e579
xustudyxu <1812903531@qq.com>
2022-07-09 20:18:12 +0800

    :wrench:添加内容:rocket:

--- docs/studynotes/middleware/sidebar.js
+++ docs/studynotes/middleware/sidebar.js
@@ -16,6 +16,7 @@ module.exports = [
       '/studynotes/middleware/ElasticSearch/ElasticSearch_MultipleAnalyzers',
       '/studynotes/middleware/ElasticSearch/ElasticSearch_Conflict_problem_handling',
       '/studynotes/middleware/ElasticSearch/ElasticSearch_Framework_integration',
+      '/studynotes/middleware/ElasticSearch/ElasticSearch_optimization',
     ]
   }
  
