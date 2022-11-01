3d06f90774efc44c08f19b33b7c9eee27e990d1a
xustudyxu <1812903531@qq.com>
2022-11-01 22:50:11 +0800

    :wrench:添加内容:rocket:

--- docs/baodian/sidebar.js
+++ docs/baodian/sidebar.js
@@ -41,7 +41,7 @@ module.exports = [
     '/baodian/high/SYT/SYT_dataInterface',
     '/baodian/high/SYT/SYT_Background_system',
     '/baodian/high/SYT/SYT_gateway',
-    '/baodian/high/SYT/SYT_client',
+    '/baodian/high/SYT/SYT_client'
     ]
   }
   
