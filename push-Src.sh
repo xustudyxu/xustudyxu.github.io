15016337247db9f7bd447119d35674008e7e876e
sx_fengrongxu <1812903531@qq.com>
2023-07-10 17:06:40 +0800

    添加 vercel 配置文件，让js、css在浏览器中采用强缓存，优化页面打开速度

--- push-Src.sh
+++ push-Src.sh
@@ -1,3 +1,4 @@
 git add -A
-git commit -m ":wrench:添加内容:rocket:"
+#git commit -m ":wrench:添加内容:rocket:"
+git commit -m "添加 vercel 配置文件，让js、css在浏览器中采用强缓存，优化页面打开速度"
 git push -f git@github.com:xustudyxu/xustudyxu.github.io.git master
